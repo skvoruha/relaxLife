3d9f2b8e233e25cf6316fe169b717065309a5302
skvoruha <skvoruha@mail.ru>
2022-03-22 15:08:36 +0300

    add admin js

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -3,12 +3,23 @@ import Swiper, {Navigation,Pagination} from 'swiper';
 
 Swiper.use([Navigation,Pagination]);
 const  repairTypesNavItem = document.querySelectorAll('.repair-types-nav__item ')
+// кнопки слайдера
+const arrowRightRepair = document.getElementById('repair-types-arrow_right')
+const arrowLeftRepair = document.getElementById('repair-types-arrow_left')
+// стрелки навигации при экране меньше 576 px
+const arrowRightNav = document.getElementById('nav-arrow-repair-right_base')
+const arrowLeftNav = document.getElementById('nav-arrow-repair-left_base')
+
+
+const getClassFromId = (str) =>{
+ return str.className.trim().replace(/\s/g, '.')
+}
 
 export const swiper = () =>{
   const repairTypesSlider = new Swiper('.repair-types-slider', {
     navigation: {
-      nextEl: ".slider-arrow_right",
-      prevEl: ".slider-arrow_left",
+      nextEl: `.${getClassFromId(arrowRightRepair)}`,
+      prevEl: `.${getClassFromId(arrowLeftRepair)}`,
     },
     pagination: {
       el: '.nav-list.nav-list-repair',
@@ -27,47 +38,40 @@ export const swiper = () =>{
     },
   });
 
-  if (window.innerWidth < 1024) {
+  if (window.innerWidth < 768) {
+    // nav-arrow-repair-right_base
+
     const  repairListRepair = document.querySelector('.nav-list-repair')
+    const svgAll = repairListRepair.querySelectorAll('svg')
+    svgAll.forEach(e => {
+      e.style.left = '30%'
+    });
+    if (window.innerWidth < 576) {
+      svgAll.forEach(e => {
+        e.style.left = '13%'
+      });
+    }
     repairListRepair.style.flexWrap = 'nowrap';
     repairListRepair.style.minWidth = 'fit-content';
     repairListRepair.style.justifyContent = 'space-evenly'
-    // no-wrap
-    // mmih -fit-content
-    // repairTypesNavItem.classList = 'nav-list nav-list-repair'
+
     const navRepairTypesNav = new Swiper('.nav.repair-types-nav', {
 
-    // конец свайпера для мобильной версии
       slideClass:'repair-types-nav__item',
       wrapperClass: 'nav-list-repair',
       navigation: {
-        nextEl: ".nav-arrow.nav-arrow_right.desktop-hide",
-        prevEl: ".nav-arrow.nav-arrow_left.desktop-hide",
+      nextEl: `.${getClassFromId(arrowRightNav)}`,
+        prevEl: `.${getClassFromId(arrowLeftNav)}`,
       },
       breakpoints: {
-        // when window width is >= 320px
         320: {
           slidesPerView: 1,
-        },
-        // when window width is >= 576px
-        576: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window width is >= 768px
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 30
         }
-
       },
       pagination: {
-          // el: ".swiper-pagination",
           type: "fraction",
       },
-
     })
-  // конец условия if
   }
 
 }
