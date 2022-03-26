81490b1db3b9e5a35a075fe73f78293a6d491e1c
skvoruha <skvoruha@mail.ru>
2022-03-26 23:34:38 +0300

    on admin css delete id and size

--- src/modules/popupThank.js
+++ src/modules/popupThank.js
@@ -1,23 +1,32 @@
 export const popupThank = () =>{
 
+  document.body.style.overflow = 'hidden'
+
   const activePopup = document.querySelector('.visibility-visible')
   if (activePopup) {
     activePopup.classList.remove('visibility-visible')
   }
 
   const popupThank = document.querySelector('.popup-thank')
-  document.body.style.overflow = 'hidden'
+
   popupThank.classList.add('visibility-visible')
 
   popupThank.addEventListener('click',(e)=>{
 
-    if (e.target.closest('.close-thank') ||
+  if (e.target.closest('.close-thank') ||
        (!e.target.closest('.feedback-wrap') &&
        popupThank.classList.contains('visibility-visible'))) {
         document.body.style.overflow = ''
       popupThank.classList.remove('visibility-visible')
     }
   })
+
+  for (let i = 0; i < 20; i++) {
+    const element = document.getElementById(`checkbox${i}`)
+    if (element) {
+      element.checked = false;
+    }
+  }
 
   setTimeout(() => {
     document.body.style.overflow = ''
